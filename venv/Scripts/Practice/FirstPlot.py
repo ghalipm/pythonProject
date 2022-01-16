58d67c2e539c2599b7c024422f59db2946d2cb5b
ghalipm(Ghalip Muhammed -  Alip Mohammed - Maimaiti Alifu ) <ghalipm@gmail.com>
2022-01-16 18:27:16 -0500

    with two ylabels

--- venv/Scripts/Practice/FirstPlot.py
+++ venv/Scripts/Practice/FirstPlot.py
@@ -11,13 +11,17 @@ for x in range(-10,11,1):
     XList.append(x)
     YList.append(y)
 plt.plot(XList,YList)
-...
+plt.title("Parabola")
+#plt.xlabel("X")
+plt.legend(['Upper Parabola'])
+#plt.show()
 
+# parabola with data points
 x=[-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10]
 y=[100,81,64,49,36,25,16,9,4,1,0,1,4,9,16,25,36,49,64,81,100]
 plt.plot(x,y)
-plt.title("First Plot")
+#plt.title("Lower Parabola")
 plt.xlabel("x")
 plt.ylabel("y")
-plt.legend(['y line'])
+plt.legend(['y=x**2', 'y=x**2+10'])
 plt.show()
