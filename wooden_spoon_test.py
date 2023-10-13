f9d2e365f9e5fb569a4f02148be981a2daa72b4b
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2023-10-13 13:27:55 -0400

    Update wooden_spoon_test.py
    finding user profile on google scholar

--- wooden_spoon_test.py
+++ wooden_spoon_test.py
@@ -40,3 +40,24 @@ def test_search_wooden_spoon(browser):
     # You can add more assertions based on the search results page
     assert "Wooden Spoon" in browser.page_source
     # it seems python is very much readable
+    
+
+    # find the profile of user "Alip Mohammed" on googlescholar
+    def test_search_user_profile(browser):
+    browser.get("https://scholar.google.com/")
+
+    # Wait for the search bar to be present
+    WebDriverWait(browser, 10).until(
+        ec.presence_of_element_located((By.ID, "gs_hdr_tsi"))
+    )
+
+    # Locate the search bar and enter "Alip Mohammed"
+    user_name = 'Alip Mohammed'
+    search_bar = browser.find_element("id", "gs_hdr_tsi")
+    search_bar.send_keys(user_name)
+    search_bar.send_keys(Keys.RETURN)
+
+    # You can add more assertions based on the search results page
+    assert user_name in browser.page_source
+
+
